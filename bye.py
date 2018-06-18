10d51e43623e0d57e1fd8ce3d0a45cb2e53cf0c4
joshlsastro <40368014+joshlsastro@users.noreply.github.com>
2018-06-18 15:54:59 -0600

    Update bye.py
    Includes alg2coords.

--- bye.py
+++ bye.py
@@ -1,7 +1,29 @@
+def alg2coords(text):
+    """Converts algebraic notation to list [x,y]."""
+    x=text[0].lower()
+    y=text[1]
+    let2coords={"a":0,
+                "b":1,
+                "c":2,
+                "d":3,
+                "e":4,
+                "f":5,
+                "g":6,
+                "h":7}
+    y=int(y)-1 #a1 maps to [0,0] not [0,1]
+    return [let2coords[x],y]
+
+def coords2alg(coords):
+    """Converts coordinates s a list [x,y] to algebraic notation."""
+    x=coords[0]
+    y=coords[1]
+    x=chr(x+65).lower()
+    y=str(y+1)
+    return x+y
+
 pos = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]]
-k_cor = []
-k_cor.append(input("What is the starting column for your piece? (letters A-H)").upper())
-k_cor.append(int(input("What is the starting row for your piece? (letters 1-8)"))-1)      #gets the starting coordinate values of  the knight in [letter,number] notation
+place=input("What coordinates do you want? ")
+k_cor=alg2coords(place)
 move = []
 board = []
 for i in range(64):
