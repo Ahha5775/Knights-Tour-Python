ee2c35f1897741bf5a2c7cb22bc849a66d944199
Ahha5775 <ahha5775@gmail.com>
2018-06-19 09:26:44 -0600

    Put everything into one file
    Added everything into bye.py as functions. Deleted files that i got those functions from, put board.py into list of lists because it's a list of lists

--- bye.py
+++ bye.py
@@ -1,3 +1,18 @@
+#NOTE: assumes board already exists and is only compatible with board as list of coordinates!
+def coor_sub(a,b):
+    """Subtracts coordinate list b from coordinate list a."""
+    return [a[0]-b[0],a[1]-b[1]]
+
+def test_legal(move,k_cor):
+    """Checks proposed move to chess coordinates on chessboard for the knight at k_cor.
+    returns True if legal and False if illegal or already visited."""
+    global board
+    poss=[[1,2],[1,-2],[-1,2],[-1,-2],
+          [2,1],[2,-1],[-2,1],[-2,-1]]
+    if coor_sub(k_cor,move) in poss:
+        if move in board:
+            return True
+    return False
 def alg2coords(text):
     """Converts algebraic notation to list [x,y]."""
     x=text[0].lower()
